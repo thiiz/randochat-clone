787914205464059c6c7ebed9eac4719a908ddfcc
thiizz <afffvitu@gmail.com>
2025-12-05 02:57:37 -0300

    chore: remove a file.

--- src/components/heartbeat-provider.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import { useHeartbeat } from '@/hooks/use-heartbeat';
-
-export function HeartbeatProvider({ children }: { children: React.ReactNode }) {
-  useHeartbeat();
-  return <>{children}</>;
-}
